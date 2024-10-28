806e4bddcd2105e045adf1993428d87c7027bbd2
batireedui1026 <136432372+batireedui1026@users.noreply.github.com>
2024-10-28 13:43:53 +0800

    service duusgah

--- client_frontend/src/app/ourService/page.tsx
+++ client_frontend/src/app/ourService/page.tsx
@@ -39,6 +39,20 @@ const OurServices = () => {
           diam venenatis platea.
         </p>
       </div>
+      <div>
+        <div
+          className="mt-5 mr-4 bg-image"
+          style={{
+            backgroundImage:
+              "url('https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/elementor/thumbs/young-handsome-man-with-blond-hair-wearing-gray-sh-2021-08-27-18-01-56-utc-e1663293082778-putr6cjso5hb5chtk3uf2x383651o3yxf4c37o8r9c.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            width: "360px",
+            height: "480px",
+          }}
+        ></div>
+      </div>
+
       <div className="pt-20 max-w-[1500px] mx-auto">
         <p className="text-4xl text-center">Үнэ & Үйлчилгээ</p>
         <p className="w-[1500px] text-center">
@@ -79,130 +93,105 @@ const OurServices = () => {
         />
       </div>
 
-      <div>
-        <p className="text-3xl text-center py-5">Эрэгтэй үс засалт</p>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Засалт</p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>50000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Улаар хусах</p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>30000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Хүүхэд</p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>40000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Хими</p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>80000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Сахал засах</p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>30000₮</p>
-        </div>
-        <p className="text-3xl text-center py-5">Эмэгтэй үс засалт</p>
-
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Тайралт</p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>50000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Хүүхэд</p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>40000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Сэнс, Индүү</p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>40000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Ороолт</p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>50000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Гоёлын засалт </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>60000₮-100000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Сүлжээс </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>60000₮-100000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Маникьюр </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>100000₮-150000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Хими дижитал </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>150000₮-250000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Хими арчих </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>80000₮</p>
+      <div className=" flex justify-between max-w-[1400px] mx-auto my-16">
+        <div className="w-[300px] h-[500px] bg-[#ECE8DE] border border-gray-300 rounded-xl">
+          <Image
+            className="mx-auto mt-5"
+            src="https://gentlecut.dropletthemes.com/wp-content/uploads/2018/12/Head-Icon-1-66x102.png"
+            width={66}
+            height={102}
+            alt="barber"
+          />
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Засалт</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">50000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Улаар хусах</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">30000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Хүүхэд</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">40000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Хими</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">80000₮</p>
+          </div>
         </div>
 
-        <p className="text-3xl text-center py-5">Гоо сайхан</p>
+        <div className="w-[300px] h-[500px] bg-[#ECE8DE] border border-gray-300 rounded-xl">
+          <Image
+            className="mx-auto mt-5 object-cover overflow-hidden "
+            src="https://files.oaiusercontent.com/file-eo54AuJ1cQbV12idYY99hMZG?se=2024-10-28T03%3A38%3A13Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D4a85fbea-73ed-420f-9b51-52c9db328fff.webp&sig=JgjZGqLMS6N3YXUlYUQHew/K0L1ceuWzLTtCXtTbCEM%3D"
+            width={66}
+            height={102}
+            alt="barber"
+          />
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Тайралт</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">50000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Хүүхэд</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">40000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Сэнс, Индүү</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">40000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Ороолт</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">50000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Гоёлын засалт</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">60000₮-100000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Сүлжээс</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">60000₮-100000₮</p>
+          </div>
+        </div>
 
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Усан массаж </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>70000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Тосон массаж </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>60000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Хими арчих </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>80000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Батганы эмчилгээ </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>45000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Нүүр будалт энгийн </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>40000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Сормуус суулгах </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>70000₮</p>
-        </div>
-
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Нүүр будалт гоёлын </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>50000₮</p>
-        </div>
-
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Сормуус нөхөлт </p>
-          <div className="flex-grow mx-4 border-b-4 border-dotted border-gray-400"></div>
-          <p>55000₮</p>
-        </div>
-        <div className="flex justify-between max-w-[1500px] mx-auto px-8 md:px-20 text-3xl">
-          <p className="text-[#F63D68]">Сормуус наах</p>
-          <div className="flex-grow mx-4 border-b-4 gap-1 border-dotted border-gray-400"></div>
-
-          <p>80000₮</p>
+        <div className="w-[300px] h-[500px] bg-[#ECE8DE] border border-gray-300 rounded-xl">
+          <Image
+            className="mx-auto mt-5 object-cover overflow-hidden  "
+            src="https://plus.unsplash.com/premium_photo-1661290231745-15f1ed6fea88?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            width={76}
+            height={102}
+            alt="barber"
+          />
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Усан массаж </p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">50000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Тосон массаж</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">30000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Хими арчих</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">40000₮</p>
+          </div>
+          <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+            <p className="font-semibold text-black">Сормуус суулгах</p>
+            <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
+            <p className="text-[#F63D68]">70000₮</p>
+          </div>
         </div>
       </div>
     </div>
