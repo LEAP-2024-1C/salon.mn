c23610b590ce186c016a6c63930c8129ea2dbea7
batireedui <batireeduiotgonsukh@gmail.com>
2024-10-29 08:54:15 +0800

    service section

--- client_frontend/src/app/ourService/page.tsx
+++ client_frontend/src/app/ourService/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const OurServices = () => {
   return (
     <div className="bg-[#101828] text-white font-serif">
-      <div className="flex justify-evenly items-start px-14 max-w-[1400px] mx-auto pt-14  ">
+      <div className="flex justify-evenly items-start px-14 max-w-[1400px] md:w-1/3 md:justify-evenly md: mx-auto pt-14  ">
         <Image
           className="mt-5 ml-4"
           src="https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/2022/06/brand_promo.png"
@@ -29,19 +29,20 @@ const OurServices = () => {
         />
       </div>
 
-      <div className="pt-20 max-w-[1400px] mx-auto">
+      <div className="pt-20 max-w-[600px] mx-auto">
         <p className="text-center text-4xl">
           Enhance your masculine through elegant hairstyle with us
         </p>
-        <p className="w-[1500px] text-center">
+        <p className="w-[600px] text-center pt-2">
           Dolor rhoncus, massa sit semper sit viverra eget mauris. Lacus
           faucibus gravida sit ut cursus consectetur auctor leo. Hac ut sit nisl
           diam venenatis platea.
         </p>
       </div>
-      <div>
-        <div
-          className="mt-5 mr-4 bg-image"
+      <div className="flex max-w-[1500px] mx-auto ml-28">
+        <div>
+          <div
+          className="mt-5 mr-4 bg-image rounded-xl"
           style={{
             backgroundImage:
               "url('https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/elementor/thumbs/young-handsome-man-with-blond-hair-wearing-gray-sh-2021-08-27-18-01-56-utc-e1663293082778-putr6cjso5hb5chtk3uf2x383651o3yxf4c37o8r9c.jpg')",
@@ -50,12 +51,163 @@ const OurServices = () => {
             width: "360px",
             height: "480px",
           }}
-        ></div>
+          ></div>
+          <div className="relative bottom-24 w-80 text-center">
+             <p className="font-semibold text-2xl">120 Model Undercut</p>
+             <p>
+             Adipiscing aliquam tristique dapibus ac. Elit ligula etiam fermentum in amet massa.</p>
+          </div>
+        </div>
+
+        <div>
+          <div
+          className="mt-5 mr-4 bg-image rounded-xl"
+          style={{
+            backgroundImage:
+              "url('https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/elementor/thumbs/handsome-man-in-grey-t-shirt-isolated-on-white-2021-08-29-11-58-46-utc-e1663294765947-putseai66bmvx2nxylnj5bvzunjsipf6gma9wd36dc.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            width: "360px",
+            height: "480px",
+          }}
+          ></div>
+          <div className="relative bottom-24 w-80 text-center">
+             <p className="font-semibold text-2xl">120 Model Undercut</p>
+             <p>
+             Adipiscing aliquam tristique dapibus ac. Elit ligula etiam fermentum in amet massa.</p>
+          </div>
+        </div>
+
+        <div>
+          <div
+          className="mt-5 mr-4 bg-image rounded-xl"
+          style={{
+            backgroundImage:
+              "url('https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/elementor/thumbs/young-handsome-man-with-curly-blond-hair-2021-08-27-18-19-01-utc-e1663292943490-putr2pw8a6igbjrlr1d9yc25im10y6k8l5nli7mha8.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            width: "360px",
+            height: "480px",
+          }}
+          ></div>
+          <div className="relative bottom-24 w-80 text-center">
+             <p className="font-semibold text-2xl">120 Model Undercut</p>
+             <p>
+             Adipiscing aliquam tristique dapibus ac. Elit ligula etiam fermentum in amet massa.</p>
+          </div>
+        </div>
+        
+        
+
+      </div>
+      <div className="flex max-w-[1500px] mx-auto ml-28">
+        <div>
+          <div
+          className="mt-5 mr-4 bg-image rounded-xl"
+          style={{
+            backgroundImage:
+              "url('https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/elementor/thumbs/face-of-happy-young-handsome-man-with-curly-hair-t-2021-08-27-15-50-33-utc-e1663294316186-puts2kgmxpl545ozf181jse14nbzilvp6ld9f4gxz4.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            width: "360px",
+            height: "480px",
+          }}
+          ></div>
+          <div className="relative bottom-24 w-80 text-center">
+             <p className="font-semibold text-2xl">120 Model Undercut</p>
+             <p>
+             Adipiscing aliquam tristique dapibus ac. Elit ligula etiam fermentum in amet massa.</p>
+          </div>
+        </div>
+
+        <div>
+          <div
+          className="mt-5 mr-4 bg-image rounded-xl"
+          style={{
+            backgroundImage:
+              "url('https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/elementor/thumbs/side-view-portrait-of-thinking-stylish-young-man-2021-08-26-22-32-33-utc-e1663294591440-puts9qy955ezsx9ihmyg3h5shlxxbddvu4rse7tog0.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            width: "360px",
+            height: "480px",
+          }}
+          ></div>
+          <div className="relative bottom-24 w-80 text-center">
+             <p className="font-semibold text-2xl">120 Model Undercut</p>
+             <p>
+             Adipiscing aliquam tristique dapibus ac. Elit ligula etiam fermentum in amet massa.</p>
+          </div>
+        </div>
+
+        <div>
+          <div
+          className="mt-5 mr-4 bg-image rounded-xl"
+          style={{
+            backgroundImage:
+              "url('https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/elementor/thumbs/face-of-young-handsome-man-with-curly-hair-2021-08-27-19-09-47-utc-e1663294893256-putshmtih47l75t6g1ns0hizvz2rvyotl5sfbs4s8w.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            width: "360px",
+            height: "480px",
+          }}
+          ></div>
+          <div className="relative bottom-24 w-80 text-center">
+             <p className="font-semibold text-2xl">120 Model Undercut</p>
+             <p>
+             Adipiscing aliquam tristique dapibus ac. Elit ligula etiam fermentum in amet massa.</p>
+          </div>
+        </div>
+        
+        
+
+      </div>  
+
+      <div>
+        <p className="text-4xl text-center">Our tools set</p>
+        <div className="flex gap-3 my-8 ml-4">
+          <div>
+            <Image className="overflow-hidden" src="https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/2022/06/hair-nose-ear-and-shaving-wireless-clipper-2021-08-26-16-25-43-utc_isolated.png"
+            width={240}
+            height={300}
+            alt="Picture of the author"
+            />
+            <p className="text-2xl w-80">Package Hair Cutting Machine by Barber 97
+            Aliquet blandit eget dolor.</p>
+          </div>
+          <div>
+            <Image className="overflow-hidden" src="https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/2022/06/professional-haircutting-scissors-isolated-2021-08-26-17-07-22-utc_isolated.png"
+            width={240}
+            height={150}
+            alt="Picture of the author"
+            />
+            <p className="text-2xl w-80">Package Hair Cutting Machine by Barber 97
+            Aliquet blandit eget dolor.</p>
+          </div>
+          <div>
+            <Image className="overflow-hidden" src="https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/2022/06/hairclipper-on-white-background-2021-08-26-18-17-31-utc_isolated.png"
+            width={240}
+            height={150}
+            alt="Picture of the author"
+            />
+            <p className="text-2xl w-80">Package Hair Cutting Machine by Barber 97
+            Aliquet blandit eget dolor.</p>
+          </div>
+          <div>
+            <Image className="overflow-hidden" src="https://elementorkits.nathatype.com/barber97/wp-content/uploads/sites/30/2022/06/hairdresser-tool-set-in-open-bag-2021-09-03-21-13-00-utc_isolated.png"
+            width={240}
+            height={300}
+            alt="Picture of the author"
+            />
+            <p className="text-2xl w-80">Package Hair Cutting Machine by Barber 97
+            Aliquet blandit eget dolor.</p>
+          </div>
+        </div>
+        
       </div>
 
       <div className="pt-20 max-w-[1500px] mx-auto">
         <p className="text-4xl text-center">Үнэ & Үйлчилгээ</p>
-        <p className="w-[1500px] text-center">
+        <p className="w-[1000px] mx-auto text-center">
           Tortor neque mi sagittis eleifend donec lectus velit, viverra. Nibh at
           nibh dolor tristique ut erat elementum. Facilisis proin semper
           tristique cidunt imperdiet velit strum donec a a in ut.
@@ -94,14 +246,15 @@ const OurServices = () => {
       </div>
 
       <div className=" flex justify-between max-w-[1400px] mx-auto my-16">
-        <div className="w-[300px] h-[500px] bg-[#ECE8DE] border border-gray-300 rounded-xl">
-          <Image
+        <div className="w-[300px] h-[500px] bg-[#ECE8DE] border-2   border-red-300 rounded-xl">
+          {/* <Image
             className="mx-auto mt-5"
             src="https://gentlecut.dropletthemes.com/wp-content/uploads/2018/12/Head-Icon-1-66x102.png"
             width={66}
             height={102}
             alt="barber"
-          />
+          /> */}
+          <p className="text-center text-2xl text-red-600">Эрэгтэй</p>
           <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
             <p className="font-semibold text-black">Засалт</p>
             <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
@@ -125,13 +278,14 @@ const OurServices = () => {
         </div>
 
         <div className="w-[300px] h-[500px] bg-[#ECE8DE] border border-gray-300 rounded-xl">
-          <Image
+          {/* <Image
             className="mx-auto mt-5 object-cover overflow-hidden "
-            src="https://files.oaiusercontent.com/file-eo54AuJ1cQbV12idYY99hMZG?se=2024-10-28T03%3A38%3A13Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D4a85fbea-73ed-420f-9b51-52c9db328fff.webp&sig=JgjZGqLMS6N3YXUlYUQHew/K0L1ceuWzLTtCXtTbCEM%3D"
+            src="https://files.oaiusercontent.com/file-Y8gXqIHzNfNxl3Ec5hgppRiX?se=2024-10-28T12%3A20%3A50Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Df01ace71-0610-4179-b4c7-5e4a828f1ad6.webp&sig=Sb/BRS3DiFvNplEuiGIrYItSxWS0cEQ5MlZoDNAi8wI%3D"
             width={66}
             height={102}
             alt="barber"
-          />
+          /> */}
+          <p className="text-center text-2xl text-red-600">Эмэгтэй</p>
           <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
             <p className="font-semibold text-black">Тайралт</p>
             <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
@@ -165,13 +319,8 @@ const OurServices = () => {
         </div>
 
         <div className="w-[300px] h-[500px] bg-[#ECE8DE] border border-gray-300 rounded-xl">
-          <Image
-            className="mx-auto mt-5 object-cover overflow-hidden  "
-            src="https://plus.unsplash.com/premium_photo-1661290231745-15f1ed6fea88?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            width={76}
-            height={102}
-            alt="barber"
-          />
+        <p className="text-center text-2xl text-red-600">Гоо сайхан </p>
+         
           <div className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
             <p className="font-semibold text-black">Усан массаж </p>
             <div className="flex-grow mx-2 border-b-2 border-dotted  border-red-400 pt-2"></div>
