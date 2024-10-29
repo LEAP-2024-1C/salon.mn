a743d3f97b58047c62855b11c3f8fcff3c883d61
batireedui1026 <136432372+batireedui1026@users.noreply.github.com>
2024-10-29 13:40:38 +0800

    bookin ,cart modal

---
+++ backend/src/models/cart.model.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+
+interface ICart {
+  _id: Schema.Types.ObjectId;
+  user: Schema.Types.ObjectId;
+  product: Schema.Types.ObjectId;
+  totalAmount: number;
+}
+
+const cartSchema = new Schema<ICart>({
+  _id: Schema.Types.ObjectId,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  totalAmount: {
+    type: Number,
+    default: 0,
+  },
+});
+const Cart = model<ICart>("Cart", cartSchema);
+export default Cart;
