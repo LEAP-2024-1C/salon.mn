bda9a516716e452791d7b118546059d56434a306
Unukuu <ll040299@gmail.com>
2024-10-30 16:57:42 +0800

    bug fixed login

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
-const TOKENPASS = process.env.JWT_TOKEN_PASSWORD || "";
+const JWT_TOKEN_PASSWORD = process.env.JWT_TOKEN_PASSWORD || "";
 export const generateToken = (payload: object) => {
-  return jwt.sign(payload, TOKENPASS, {
+  return jwt.sign(payload, JWT_TOKEN_PASSWORD, {
     expiresIn: "1h",
   });
 };
 export const decodeToken = (token: string) => {
-  return jwt.verify(token, TOKENPASS);
+  return jwt.verify(token, "Pass1234@12");
 };
