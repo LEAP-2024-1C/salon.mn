6f10c1606d97733da1fe4e2ebfeede42d33062ae
bayarmaa0715 <24LP2392@G303-CC0139.local>
2024-10-30 10:41:02 +0800

    verify otp function

--- backend/src/routes/auth-router.ts
+++ backend/src/routes/auth-router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { signin } from "../controller/auth-controller";
+import { signin, verifyOtp } from "../controller/auth-controller";
 
 const router = Router();
 router.route("/signin").post(signin);
+router.route("/verifyotp").post(verifyOtp);
 export default router;
