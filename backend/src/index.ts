38705d2fc46d2751b8eebad367cc81b09af18dc1
bayarmaa0715 <24LP2392@G303-CC0139.local>
2024-10-30 18:13:27 +0800

    employee controllor nemew, index.ts deer route newem

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import proRoute from "./routes/product-route";
 import categoryRoutes from "./routes/categoryRoutes";
 import adminRoute from "./routes/adminRoutes";
 import signinRoutes from "./routes/auth-router";
+import employeeRoutes from "./routes/employee-route";
 
 dotenv.config();
 
@@ -23,6 +24,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/products", proRoute);
 app.use("/api/v1/admin", adminRoute);
 app.use("/api/v1/auth", signinRoutes);
+app.use("/api/v1/employee", employeeRoutes);
+
 connectDB(MONGO_URI);
 //server asaah
 app.listen(PORT, () => {
