e99a97f16f4ee9f72e7103329bbd8d9eb42607a5
batireedui1026 <136432372+batireedui1026@users.noreply.github.com>
2024-11-01 09:43:45 +0800

    service hiiv

--- backend/src/controller/service-controller.ts
+++ backend/src/controller/service-controller.ts
@@ -2,11 +2,9 @@ import { Request, Response } from "express";
 import Service from "../models/service.model";
 
 export const getService = async (req: Request, res: Response) => {
-  const { price, category, time, description } = req.body;
-
   try {
-    const service = await Service.find({ price, category, time, description });
-    res.status(200).json({ message: "success to get all  services" });
+    const service = await Service.find({});
+    res.status(200).json({ message: "success to get all  services", service });
   } catch (error) {
     console.log(error);
     res.status(400).json({ message: "error to get all services" });
