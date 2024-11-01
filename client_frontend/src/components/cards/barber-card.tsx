d164fbc31ae4144f5aa4b1e2c2d6b651b3a2cb73
dl munkhod <dlmunkhod@dls-MacBook-Air.local>
2024-11-01 12:33:07 +0800

    bg ungu uurchilluu

--- client_frontend/src/components/cards/barber-card.tsx
+++ client_frontend/src/components/cards/barber-card.tsx
@@ -12,7 +12,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const BarberCard = ({name, selected}: {name: string, selected: boolean}) => {
   return (
-    <Card className={`w-[150px] m-auto flex flex-col items-center px-3 py-5 shadow-lg ${selected && 'bg-black'}`}>
+    <Card className={`w-[150px] m-auto flex flex-col items-center px-3 py-5 shadow-lg ${selected ? 'bg-black' : 'bg-white'}`}>
       <CardContent className="w-full flex flex-col gap-4 items-center p-0">
         
         <div className={`w-20 h-20 p-3 rounded-full flex justify-center items-center ${selected ? 'bg-white text-black': 'bg-black text-white'}`}>
