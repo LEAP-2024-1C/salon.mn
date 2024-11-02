e1c0972815cb53c87df85f1b9ed802589365af3a
bayarmaa0715 <24LP2392@G303-CC0139.local>
2024-11-02 13:19:13 +0800

    deploey

---
+++ backend/build/routes/adminRoutes.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const admin_controller_1 = require("../controller/admin-controller");
+const router = (0, express_1.Router)();
+router.route("/").post(admin_controller_1.createAdmin);
+router.route("/login").post(admin_controller_1.login);
+exports.default = router;
