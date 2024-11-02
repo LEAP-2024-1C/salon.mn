75f51fcd6366b3a107d739dd9ae8ae89e7562298
bayarmaa0715 <bayrmaa.m49@gmail.com>
2024-11-03 02:39:18 +0800

    product crud deploe

--- admin/app/dashboard/product/page.tsx
+++ admin/app/dashboard/product/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ProductContext } from '@/app/context/product-context';
+
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
 import { ProductTable } from '@/components/tables/product-tables/product-table';
@@ -10,7 +10,6 @@ import { Separator } from '@/components/ui/separator';
 import { cn } from '@/lib/utils';
 import { Plus } from 'lucide-react';
 import Link from 'next/link';
-import { useContext } from 'react';
 
 const breadcrumbItems = [
   { title: 'Dashboard', link: '/dashboard' },
@@ -18,18 +17,13 @@ const breadcrumbItems = [
 ];
 
 export default async function page() {
-  const { products } = useContext(ProductContext);
-
   return (
     <PageContainer>
       <div className="space-y-4">
         <Breadcrumbs items={breadcrumbItems} />
 
         <div className="flex items-start justify-between">
-          <Heading
-            title={`Products (${products.length})`}
-            description="Manage products (Server side table functionalities.)"
-          />
+          <Heading title={`Products`} description="Manage products" />
 
           <Link
             href={'/dashboard/product/create'}
