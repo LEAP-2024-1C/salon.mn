cb963468e4c7596c19f731e952f48494bd3bf155
bayarmaa0715 <24LP2392@G303-CC0139.local>
2024-11-07 19:16:27 +0800

    sut cat nemew

--- client_frontend/src/app/ourService/page.tsx
+++ client_frontend/src/app/ourService/page.tsx
@@ -1,25 +1,24 @@
-"use client"
-import axios from "axios";
+"use client";
+
 // import Image from "next/image";
-import { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
 
 const OurServices = () => {
-  const [service, setService] = useState<any[]>([]);
+  // const [service, setService] = useState<any[]>([]);
 
-  const getService = async () => {
-    try {
-      const res = await axios.get(`http://localhost:8008/api/v1/service`);
-      setService(res.data); 
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const getService = async () => {
+  //   try {
+  //     const res = await axios.get(`http://localhost:8008/api/v1/service`);
+  //     setService(res.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    getService();
-  }, []);
-
-  console.log("service", service)
+  // useEffect(() => {
+  //   getService();
+  // }, []);
 
   return (
     <div className="bg-[#101828] text-white font-serif">
@@ -33,16 +32,16 @@ const OurServices = () => {
       <div className="block md:flex md:justify-between md:items-center md:text-xl md:gap-2 lg:max-w-[1200px] lg:mx-auto lg:py-8 animate-fadeInUp5">
         <div className="w-[300px] h-[500px] bg-[#ECE8DE] border-2 border-red-300 rounded-xl">
           {/* <p className="text-center text-2xl text-red-600">Эрэгтэй</p> */}
-          {service.services?.map((item) => (
-            <div key={item.id} className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4">
+          {/* {service?.map((item) => (
+            <div
+              key={item.id}
+              className="flex items-center justify-between w-[270px] mx-auto mt-6 text-xl text-white px-4"
+            >
               <p className="font-semibold text-black">{item.name}</p>
               <p className="text-[#F63D68]">{item.price}</p>
-              
             </div>
-          ))}
+          ))} */}
         </div>
-
-      
       </div>
     </div>
   );
